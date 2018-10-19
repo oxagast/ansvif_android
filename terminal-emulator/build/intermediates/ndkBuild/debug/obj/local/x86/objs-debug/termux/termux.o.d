53bf3ddba1cf042d3497871161e34b47c46fe8dd
Marshall Lee Whittaker <marshall@likon.maskster.site>
2018-10-19 01:26:32 -0400

    Getting ready for an initial android release, termux base.

---
+++ terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/termux/termux.o.d
@@ -0,0 +1,2 @@
+/home/marshall/AndroidStudioProjects/termux-app/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/termux/termux.o: \
+  /home/marshall/AndroidStudioProjects/termux-app/terminal-emulator/src/main/jni/termux.c
